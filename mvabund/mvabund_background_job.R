e40116fd420ac6e4a6081c6ea1c644b3cd50e6af
Marissa Wright-LaGreca <98763283+MarissaLag@users.noreply.github.com>
2024-05-30 09:54:04 -0700

    asv numbers

--- mvabund/mvabund_background_job.R
+++ mvabund/mvabund_background_job.R
@@ -54,8 +54,11 @@ pseq@sam_data$Family <- as.character(pseq@sam_data$Family)
 # testing
 taxa_to_subset <- c("ASV21", "ASV34", "ASV40", "ASV42", "ASV44", "ASV48", "ASV51", "ASV60", "ASV76", "ASV79", "ASV90", "ASV91", "ASV92", "ASV100", "ASV109", "ASV114", "ASV118", "ASV119", "ASV124", "ASV126", "ASV130", "ASV131", "ASV150", "ASV163", "ASV176", "ASV181", "ASV187", "ASV189", "ASV193", "ASV195", "ASV216", "ASV218", "ASV219", "ASV237", "ASV240", "ASV244", "ASV257", "ASV262", "ASV309", "ASV313", "ASV315", "ASV319", "ASV326", "ASV333", "ASV337", "ASV343", "ASV349", "ASV351", "ASV356", "ASV375", "ASV379", "ASV385", "ASV387", "ASV394", "ASV397", "ASV401", "ASV416", "ASV417", "ASV428", "ASV431", "ASV447", "ASV450", "ASV468", "ASV484", "ASV487", "ASV499", "ASV501", "ASV510", "ASV523", "ASV524", "ASV538", "ASV556", "ASV559", "ASV563", "ASV575", "ASV579", "ASV595", "ASV608", "ASV620", "ASV641", "ASV649", "ASV652", "ASV703", "ASV738", "ASV756", "ASV758", "ASV789", "ASV798", "ASV818", "ASV819", "ASV820", "ASV837", "ASV838", "ASV845", "ASV846", "ASV872", "ASV874", "ASV882", "ASV889", "ASV916", "ASV918", "ASV919", "ASV929", "ASV952", "ASV955", "ASV960", "ASV982", "ASV983", "ASV993", "ASV1005", "ASV1007", "ASV1030", "ASV1035", "ASV1036", "ASV1061", "ASV1070", "ASV1074", "ASV1078", "ASV1082", "ASV1086", "ASV1110", "ASV1111", "ASV1138", "ASV1146", "ASV1153", "ASV1170")
 #taxa_to_subset <- taxa_to_subset[1:11]
-taxa_to_subset <- taxa_to_subset[1:50]
+#taxa_to_subset <- taxa_to_subset[1:50]
+#taxa_to_subset <- taxa_to_subset[1:100]
 #taxa_to_subset <- taxa_to_subset[1:200]
+taxa_to_subset <- taxa_to_subset[1:400]
+
 
 # Spat significant ASVs (without a p-value adjustment)
 # taxa_to_subset <- c("ASV17",  "ASV30",   "ASV37",   "ASV49",   "ASV216",  "ASV237",  "ASV240",  "ASV254",  "ASV271",  "ASV314", "ASV325", "ASV347"
@@ -91,44 +94,44 @@ fact$numberReads <- rowSums(ASV_data_cleaned)
 dat_mvabund <- mvabund(ASV_data_cleaned)
 
 #Run manyglms ----
-<<<<<<< Updated upstream
-#VERY slow - ways to speed it up? Trying parallel processing
-
-# Define the number of cores you want to use
-num_cores <- detectCores() - 1  # Use one less than the total number of cores
-
-# Create a cluster
-cl <- makeCluster(num_cores)
-
-# Define the model and anova functions
-run_model <- function(dat_mvabund, fact) {
-  manyglm(dat_mvabund ~ Treatment * Family, family = "negative.binomial", data = fact, composition = TRUE)
-}
-
-run_anova <- function(run_model) {
-  anova(run_model, p.uni = "unadjusted")
-}
-
-# Export necessary objects and libraries to the cluster
-clusterExport(cl, c("dat_mvabund", "fact", "run_model", "run_anova"))
-clusterEvalQ(cl, library(mvabund))
-
-# Run the model and ANOVA in parallel
-results <- parLapply(cl, 1:num_cores, function(x) {
-  model <- run_model(dat_mvabund, fact)
-  anova_result <- run_anova(model)
-  return(anova_result)
-})
-
-# Stop the cluster
-stopCluster(cl)
-
-# Combine or analyze the results as needed
-# Here, we're assuming that the first result is representative (if they are all the same)
-dat_aov_unadj_Spat <- results[[1]]
-=======
-# note: VERY slow - ways to speed it up? Trying parallel processing
->>>>>>> Stashed changes
+# <<<<<<< Updated upstream
+# #VERY slow - ways to speed it up? Trying parallel processing
+# 
+# # Define the number of cores you want to use
+# num_cores <- detectCores() - 1  # Use one less than the total number of cores
+# 
+# # Create a cluster
+# cl <- makeCluster(num_cores)
+# 
+# # Define the model and anova functions
+# run_model <- function(dat_mvabund, fact) {
+#   manyglm(dat_mvabund ~ Treatment * Family, family = "negative.binomial", data = fact, composition = TRUE)
+# }
+# 
+# run_anova <- function(run_model) {
+#   anova(run_model, p.uni = "unadjusted")
+# }
+# 
+# # Export necessary objects and libraries to the cluster
+# clusterExport(cl, c("dat_mvabund", "fact", "run_model", "run_anova"))
+# clusterEvalQ(cl, library(mvabund))
+# 
+# # Run the model and ANOVA in parallel
+# results <- parLapply(cl, 1:num_cores, function(x) {
+#   model <- run_model(dat_mvabund, fact)
+#   anova_result <- run_anova(model)
+#   return(anova_result)
+# # })
+# 
+# # Stop the cluster
+# stopCluster(cl)
+# 
+# # Combine or analyze the results as needed
+# # Here, we're assuming that the first result is representative (if they are all the same)
+# dat_aov_unadj_Spat <- results[[1]]
+# =======
+# # note: VERY slow - ways to speed it up? Trying parallel processing
+# >>>>>>> Stashed changes
 
 
 #### Parallel processing method (in development) ####
